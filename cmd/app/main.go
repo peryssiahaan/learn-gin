412771c130ce380e1f21545b28f7b6dd01fd78ed
peryssiahaan <peryssiahaan@gmail.com>
2024-05-15 20:35:54 +0700

    add command to give further description

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -16,15 +16,19 @@ func main() {
 
 	r := gin.Default()
 
+	// Api ini berguna untuk kebutuhan utama bisnis.
 	r.GET("/posts", middlewares.AuthMiddleWare, handlers.GetPostsHandler)
 	r.GET("/posts/:id", middlewares.AuthMiddleWare, handlers.GetPostHandler)
 	r.POST("/posts", middlewares.AuthMiddleWare, handlers.CreatePostHandler)
 	r.PUT("/posts/:id", middlewares.AuthMiddleWare, handlers.UpdatePostHandler)
 	r.DELETE("/posts/:id", middlewares.AuthMiddleWare, handlers.DeletePostHandler)
 
+	// Api ini berguna untuk melakukan authentikasi dan deauthentikasi.
 	r.POST("/signup", handlers.SignupHandler)
 	r.POST("/login", handlers.LoginHandler)
 	r.POST("/logout", middlewares.AuthMiddleWare, handlers.LogOutHandler)
+
+	// Api ini berguna untuk CMS.
 	r.GET("/users/:id", middlewares.AuthMiddleWare, handlers.GetUserHandler)
 	r.PUT("/users/:id", middlewares.AuthMiddleWare, handlers.UpdateUserHandler)
 	r.DELETE("/users/:id", middlewares.AuthMiddleWare, handlers.DeleteUserHandler)
